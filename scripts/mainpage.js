966d95ee4bbe5891e3a671c27f9ae51b4a136b91
Guilherme Luz <luzguilherme.dev@gmail.com>
2024-06-13 16:56:11 -0300

    style: fazendo ajustes finais

--- scripts/mainpage.js
+++ scripts/mainpage.js
@@ -1,6 +1,16 @@
-// Adicionando animação no menu Hamburguer para telas de celulares.
-$('document').ready(()=>{
-    $('.burguer_menu').on('click', ()=>{
+// Adicionando animação na página principal
+$('document').ready(() => {
+    // Animação do Menu Hamburguer
+    $('.burguer_menu').on('click', () => {
         $('.nav_menu').slideToggle();
+    })
+
+    // Animação dos botões de ver mais dos catalogos
+    $('.view_more').on('click', () => {
+        $('.hidden_list').slideToggle();
+
+    })
+    $('.view_more2').on('click', () => {
+        $('.hidden_list2').slideToggle();
     })
 })
